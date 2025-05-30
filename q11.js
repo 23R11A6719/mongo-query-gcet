845014563573cf39807c7aafedc9cc63d806b1ef
23R11A6719 <23r11a6719@gcet.edu.in>
2025-05-30 20:22:35 +0530

    update

---
+++ q11.js
@@ -0,0 +1,32 @@
+db.employees.aggregate([
+  {
+    $unwind: {
+      path: "$location",
+      preserveNullAndEmptyArrays: true
+    }
+  },
+  {
+    $project: {
+      name: 1,
+      email: 1,
+      department:1,
+      salary: 1,
+      location:1,
+      age: 1,
+      annualSalary: { $multiply: ["$salary", 12] },
+      band: {
+        $cond: {
+          if: { $gt: ["$age", 40] },
+          then: "BAND A",
+          else: "BAND B"
+        }
+      }
+    }
+  },
+  {
+    $sort: { salary: -1 }
+  },
+  {
+    $limit: 3
+  }
+]);
